1c840a1cf48d690489d8a86a3bd652919aa07887
MR-B-Star <adityabintang197@gmail.com>
2025-11-13 00:29:06 +0700

    Add ConnectDB function for PostgreSQL connection
    Implement database connection function for PostgreSQL.

--- Internal/Adapters/db/order.go
+++ Internal/Adapters/db/order.go
@@ -1,1 +1,22 @@
-package db
+package db 
+
+import (
+    "database/sql"
+    _ "github.com/lib/pq"
+    "fmt"
+)
+
+func ConnectDB() (*sql.DB, error) {
+    connStr := "postgres://postgres;Password=admin123@localhost:5432/AGRITRACEsslmode=disable"
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        return nil, fmt.Errorf("gagal membuka koneksi: %v", err)
+    }
+
+    if err := db.Ping(); err != nil {
+        return nil, fmt.Errorf("gagal menguji koneksi: %v", err)
+    }
+
+    fmt.Println("Berhasil terhubung ke PostgreSQL!")
+    return db, nil
+}
